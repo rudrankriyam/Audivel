f0e08ab0d6862257e07612bd00906cefa45dcc06
Rudrank Riyam <rudrankriyam@gmail.com>
2024-11-13 22:54:10 +0530

    Add Audivel project

---
+++ Audivel/AudivelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudivelApp.swift
+//  Audivel
+//
+//  Created by Rudrank Riyam on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AudivelApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
